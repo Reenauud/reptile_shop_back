f0cbe453fe46c560bb3b3119d6a15fb03038e454
DieumegardGregory <dieumegard.gregory@gmail.com>
2023-07-20 10:12:09 +0200

    [FIX] accès à la bdd test

--- src/tools/utils.ts
+++ src/tools/utils.ts
@@ -11,9 +11,9 @@ export const dataSource = new DataSource({
   type: "postgres",
   host: "db",
   port: 5432,
-  username: "gregrun",
-  password: "example",
-  database: "reptiles",
+  username: `${process.env.NODE_ENV === "test" ? process.env.DB_USER_TEST : process.env.DB_USERNAME}`,
+  password: `${process.env.NODE_ENV === "test" ? process.env.DB_PASSWORD_TEST : process.env.DB_PASSWORD}`,
+  database: `${process.env.NODE_ENV === "test" ? process.env.DB_NAME_TEST : process.env.DB_DATABASE}`,
   synchronize: true,
   entities: [User, Reptile, Order, Food, Equipment, Category, Upkeep],
 });
