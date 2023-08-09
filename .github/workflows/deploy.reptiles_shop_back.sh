cb0b2108a8185d0ca8bac51aa405d7c25f68835c
reenauud <renaud.raymond43@hotmail.fr>
2023-08-09 16:17:28 +0200

    github file for deployment

---
+++ .github/workflows/deploy.reptiles_shop_back.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# prune docker
+docker stop $(docker ps --filter status=running --filter name=reptile_shop_back -q)
+docker rm -f $(docker ps --filter status=exited -q)
+docker rmi -f $(docker images reptile_shop_back* -q)
+docker image prune -f
+
+# fix for "Permission denied" error
+sudo chmod -R 777 reptile_shop_back/
+
+# prepare new deployment folder
+mv reptile_shop_back/ old_reptile_shop_back/
+git clone git@github.com:Reenauud/reptile_shop_back.git
+cd reptile_shop_back/
+git pull -f --rebase origin main
+mkdir bdd/sql-files
+
+# récupérer les .env uploadés précédemment avec scp et les déplacer ici
+mv ../dotenv/.env.backend .env
+
+# move old database to new folder
+mv ../old_reptile_shop_back/dbData/ .
+# fix for "no permission to read" error
+sudo chmod -R 777 dbData/
+
+# build docker images
+docker compose -f docker-compose.yml build --no-cache
+
+# start containers
+docker compose -f docker-compose.yml up >~/logs/reptile_shop_back/log.compose.$(date +"%s") 2>&1 &
+disown
+
+# delete old folder
+sudo rm -Rf ~/old_reptile_shop_back/
